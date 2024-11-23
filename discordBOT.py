7a348b39ebce939e0e28436a92ee97a1bb91d1f2
studentAutomations <student.automations@gmail.com>
2024-11-23 16:10:49 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -5,14 +5,17 @@ from dhooks import Webhook, Embed
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 hook = Webhook(WEBHOOK_URL) 
 
+
+
 embed = Embed(
     color= 0x499957,
     timestamp='now'
 )
 
 
+hook.send('@everyone This is a notification for all members!')
+
 embed.add_field(name='SIP bot', value='[installed]')
-embed.add_field(name='Notify', value='<@everyone>')
 embed.set_footer(text='by og._.njen')
 
 
