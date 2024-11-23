fda3d146da844c21d74501af71cc6f7bc0f841fb
studentAutomations <student.automations@gmail.com>
2024-11-23 15:35:51 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -6,17 +6,13 @@ WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 hook = Webhook(WEBHOOK_URL) 
 
 embed = Embed(
-    color=0xFF4545,
+    color= 0x499957,
     timestamp='now'
 )
 
-image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
-image2 = 'https://github.com/studentAutomations/cs-lp/blob/main/cs-lp-nova-obavestenja.png'
+embed.set_author(name='Custom Bot')
+embed.add_field(name='Botovi za Sip i CS', value='[installed]')
+embed.set_footer(text='by og._.njen')
 
-embed.set_author(name='Nova obavestenja na CS-u')
-embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
-embed.add_field(name='Vidi obavestenja - ', value=image2)
-embed.set_footer(text='Elektronski Fakultet')
-embed.set_thumbnail(image1)
 
 hook.send(embed=embed)
